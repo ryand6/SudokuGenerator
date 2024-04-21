d2ae94bcebab097e6318cd3c791ee580159cde34
R D <ryandowney64@yahoo.com>
2024-04-21 20:35:46 +0100

    initial commit

---
+++ src/main/java/com/github/ryand6/SudokuCore.java
@@ -0,0 +1,7 @@
+package com.github.ryand6;
+
+public class SudokuCore {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/github/ryand6/SudokuGenerator.java
@@ -0,0 +1,8 @@
+package com.github.ryand6;
+
+public class SudokuGenerator {
+
+    public static void main(String[] args) {
+
+    }
+}
